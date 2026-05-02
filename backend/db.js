83418d5f301be78b306b5b464434acbed111a726
SharonW-wa <wanjikusharon004@gmail.com>
2026-05-02 13:26:07 +0300

    update new changes

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,11 @@ import mysql from 'mysql2/promise';
 
 // This matches your Navicat setup
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '', 
-  database: 'ecotrack',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
